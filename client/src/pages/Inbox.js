e99328d106ead0585461dcfb10f8a3b7e9cf4762
fmoncef <moncef.fahssi@gmail.com>
2022-11-03 09:43:26 +0100

    thirdCommit

--- client/src/pages/Inbox.js
+++ client/src/pages/Inbox.js
@@ -19,6 +19,28 @@ const Inbox = () => {
         explicabo mollitia harum porro? Voluptate voluptatem quidem perferendis
         velit beatae libero alias consectetur hic suscipit placeat dolorum, et
         quia sunt fugiat in culpa vel.
+        <br /> Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt
+        perspiciatis vitae pariatur ratione tempore cum nobis alias porro,
+        laboriosam in saepe quibusdam autem, aut deserunt eius non velit
+        architecto dicta amet. Rem aliquam asperiores eius eum expedita alias
+        in, labore, praesentium beatae quas possimus placeat pariatur ratione
+        cumque ex mollitia quia consequatur! Mollitia aut deleniti inventore
+        molestiae et ullam, temporibus ipsum illum, non laudantium, quos in ad
+        ex! Repudiandae sapiente possimus in consectetur odit. Nemo expedita
+        explicabo mollitia harum porro? Voluptate voluptatem quidem perferendis
+        velit beatae libero alias consectetur hic suscipit placeat dolorum, et
+        quia sunt fugiat in culpa vel.
+        <br /> Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt
+        perspiciatis vitae pariatur ratione tempore cum nobis alias porro,
+        laboriosam in saepe quibusdam autem, aut deserunt eius non velit
+        architecto dicta amet. Rem aliquam asperiores eius eum expedita alias
+        in, labore, praesentium beatae quas possimus placeat pariatur ratione
+        cumque ex mollitia quia consequatur! Mollitia aut deleniti inventore
+        molestiae et ullam, temporibus ipsum illum, non laudantium, quos in ad
+        ex! Repudiandae sapiente possimus in consectetur odit. Nemo expedita
+        explicabo mollitia harum porro? Voluptate voluptatem quidem perferendis
+        velit beatae libero alias consectetur hic suscipit placeat dolorum, et
+        quia sunt fugiat in culpa vel.
       </p>
     </div>
   );
